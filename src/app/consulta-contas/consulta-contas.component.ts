e5110336484a3af30f0c7f421d1464e36c3c3520
cleber-almeida-souza <cleberalsouza@gmail.com>
2021-04-15 21:40:17 -0300

    Desenvolvimento dos componentes e chamadas para a API - Angular

---
+++ src/app/consulta-contas/consulta-contas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-contas',
+  templateUrl: './consulta-contas.component.html',
+  styleUrls: ['./consulta-contas.component.css']
+})
+export class ConsultaContasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
